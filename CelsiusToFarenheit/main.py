c5c69084940768ff270647fea17ca66fd2013ec1
SamuelAdamson <adamson.samuel10@gmail.com>
2021-11-23 00:05:06 -0700

    Completed Celsuis to Farenheit

--- CelsiusToFarenheit/main.py
+++ CelsiusToFarenheit/main.py
@@ -5,12 +5,62 @@
 # Last Edit: 11/22/2021
 
 import tensorflow as tf
-import numpy as np
-import logging
-
+import matplotlib.pyplot as plot
 import training_data as td
+import os
 
 
-# File Execution
+# Create and Train Model
+# Return Type: Tuple
+# @RETURN: ( training history, model )
+def ct_model():
+    # Model -- l_0
+    #   -- Single Layer
+    #   -- Input is a single value, 1d array
+    l_0 = tf.keras.layers.Dense(units=1, input_shape=[1])
+    model = tf.keras.Sequential([l_0])
+
+    # Configure model for training
+    model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.1))
+    # Train Model
+    history = model.fit(td.celsius, td.farenheit, epochs=1000, verbose=False)
+    print("Model Training Complete!")
+
+    return (history, model)
+
+
+# Training Visualization
+# Return Type: None
+# @RETURN: None
+def display_training(history):
+    # Setup Plot
+    plot.xlabel('Epoch')
+    plot.ylabel('Loss')
+    plot.plot(history.history['loss'])
+
+    # # Save plot
+    # plot.savefig('./figs/training.png')
+    # Show Plot
+    plot.show()
+
+
+# Predict Value
+# Return Type: None
+# @RETURN: None
+def predict(model):
+    # Get Input From Terminal
+    celsius_in = float(input('Enter celsius value for conversion: '))
+    # Print Prediction
+    print(model.predict([celsius_in]))
+
+
+# Execution
 if __name__ == '__main__':
-    print("some")
+    # Set logging for errors only
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+    # Create and train model
+    history, model = ct_model()
+
+    # td.display_values()
+    # display_training(history)
+    # predict(model)
--- CelsiusToFarenheit/training_data.py
+++ CelsiusToFarenheit/training_data.py
@@ -2,3 +2,14 @@
 # Author: Samuel Adamson
 # Last Edit: 11/22/2021
 
+import numpy as np
+
+# Training Arrays
+celsius = np.array([-40, -30, -20, -10, 0, 5, 8, 10, 15, 22, 28], dtype=float)
+farenheit = np.array([-40,  -22, -4, 14, 32, 41, 46.4, 50, 59, 71.6, 82.4], dtype=float)
+
+# Display Training Values
+def display_values():
+    for i,c in enumerate(celsius):
+        # { Temp } Celsius = { Temp F } Farenheit
+        print("{} Celsius = {} Farenheit".format(c, farenheit[i]))
